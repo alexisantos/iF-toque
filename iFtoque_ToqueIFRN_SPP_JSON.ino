aa6819dbd7fa493c1f47b9e6e9d54391ee611044
Alex Santos <alexisantos@gmail.com>
2022-09-28 14:20:48 -0300

    Update iFtoque_ToqueIFRN_SPP_JSON.ino
    Atualização de código para Esp32

--- iFtoque_ToqueIFRN_SPP_JSON.ino
+++ iFtoque_ToqueIFRN_SPP_JSON.ino
@@ -1,30 +1,74 @@
 /*
   ACIONADOR TOQUE ESCOLAR ELETRÔNICO AUTOMÁTICO IFRN-SPP
-  - Baseado no sample TimeNTP da biblioteca Time.h
-  - Função timeToStudy desenvolvida por Diego Cirilo (https://github.com/dvcirilo/alarme_ifrn)
-
+  Funções e bibliotecas de apoio e referência
+     Função timeToStudy: https://github.com/dvcirilo/alarme_ifrn
+     Stuct tm:           https://cplusplus.com/reference/ctime/tm/
+     Esp32 NTP client:   https://randomnerdtutorials.com/esp32-date-time-ntp-client-server-arduino/
+     NTPSync Callback:   https://techtutorialsx.com/2021/09/03/esp32-sntp-additional-features/#Setting_the_SNTP_sync_interval
+     Espressif:          https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/system_time.html#_CPPv419sntp_sync_time_cb_t
+     Espressif:          https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/misc_system_api.html
+     Espressif:          https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/misc_system_api.html
+     Arduinojson:        https://arduinojson.org
   Autor: Alex Santos - CTI/SPP (Outubro/2017)
-  Último Update: Fev/2021
+  Último Update: Setembro/2022
 */
 
+#include <WiFi.h>
+#include "time.h"
 #include <TimeLib.h>
-#include <Ethernet.h>
-#include <EthernetUdp.h>
-#include <SPI.h>
+#include <ArduinoJson.h> //https://arduinojson.org
+
+#define SINETA 26 //mesma porta do led interno
+#define SINETA_TESTE 27 //SEMPRE ON
+
+#define LED_AZUL_STATUS 2 //mesma porta do led interno. Se on, hora ok, se off sem horário.
+
+// Variáveis Globais:
+// Wifi:
+const char* ssid     = "SSID";
+const char* password = "Senha wifi";
+
+// NTP
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset = -3;
+const int   daylightOffset_sec = 0;
+long int last_ntp_update = 0;
+struct tm timeinfo;
+time_t agora;
+
+//Uptime
+static uint32_t locUpTime = 0;
+uint32_t prevMillis = millis();
+
+// Para controle de aula e turno
+byte atualAula = 0;                     // Armazena a Aula atual  : 1 a 6 ou 0, 10, 20 ou 30
+byte proxAula = 0;                      // Armazena a Proxima aula: 1 a 6 (nao considera intervalo como aula)
+byte turno = 0;                         // Armazena o turno atual
+
+// Para exibicao em web: Controle de tempo para proximo toque
+tmElements_t tmRestante;                 // Estrutura que armazena (hh, mm, ss) restante para o proximo toque
+
+// Para exibicao da hora atual em hh:mm:ss
+time_t prevDisplay = 0;                 // Controla para só exibir da data/hora no display/serial caso a hora atual seja diferente da atual
+
+
+DynamicJsonDocument doc(1000);
+WiFiServer server(80);
 
 /* ---------Protótipos das funções: -------------------*/
-/*---------------------------------------------------- */
-/*   Acionamento do toque escolar - Funcões essenciais*/
-//void    (* resetFunc) (void) = 0;                             // Resseta o arduino
 boolean DiadeAula();                                            // Retorna true caso não seja sabado, domingo e feriados (Configurar feriados nesta função)
 boolean timeToStudy(byte hora, byte minuto, byte segundo);      // Retorna true se estiver na hora de acionar o toque escolar
 
+/*   Funções de conectividade wifi */
+void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info);     //Funções de Wifi
+void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info);                //Funções de Wifi
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info);  //Funções de Wifi
+
 /*   Formatação e exibicao de data/hora: */
 void    Imprime_Info_DataHora_Serial();
-String  digitalClockDisplayStringHora(byte hh, byte mm, byte ss);                 // Retorna a hora em formato hh:mm:ss
-String  digitalClockDisplayStringData(byte wday, byte DD, byte MM, int YYYY);     // Retorna a hora em formato hh:mm:ss
 String  diaSemana(byte dia);                                    // Recebe um numero (1-7) e retorna dia da semana em string (-,Domingo, Segunda, (...), Sabado)
 String  Printzero(byte a);                                      // Recebe um número
+void    printLocalTime();                                        // Exibe a data em diversos formatos
 
 /*    Exibicao de informações da aula atual */
 byte    AulaAtualNum ();                                        // Retorna um numero correspondente a aula atual: (1 a 6) ou (0, 10, 20 ou 30)
@@ -36,104 +80,82 @@ String  TurnoStr ();                                            // Retorna uma S
 String  TurnoStrRange();                                        // Retorna a duracao do turno atual: '07h as 12h', '13 as 18' ou '19h as 22h10min'
 
 /*    Funcoes que retornam tempo no formato Unix epoch/Unix timestamp  */
-time_t  getNtpTime ();                                          // Realiza a consulta de hora, retorna 0 caso não consiga obter a hora
+unsigned long getTime();                                        // retorna o horario atual em unixtime
 time_t  TempoProxAula ();                                       // Calcula o tempo para a proxima aula, retorna um epoch time deste tempo. Quebra esta hora em tmRestante.hora, min e s
 time_t  HoraEpoch (byte hh, byte mm);                           // Converte uma hora e minuto fornecida em um epoch time do dia atual. Útil para comparar com now()
 
 /*    Funcoes com rede e monitoramento*/
 void    ServidorWeb  ();                                        // Monta um servidor web, exibe as informações de hora caso a hora tenha sido obtida
-void    sendNTPpacket(IPAddress &address);                      // Envia solicitações NTP para o servidor NTP do endereço IP fornecido
-String  NTP_Status_Desc (byte timestatus);                       // Devolve status da hora em modo textual (retorna: 0 timeNotSet, 1 timeNeedsSync ou 2 timeSet)
-void    Uptime_Calc();
-/*------------------------- */
-
-/* PINOS UTILIZADOS NO ARDUINO E PORTA DE MONITORAMENTO*/
-#define SINETA 3
-#define HTTP_PORT 80
-
-/* VARIAVEIS GLOBAIS*/
-static byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xB0, 0x6D };
-static byte ip[] = { 192, 168, 51, 110};
-static byte gateway[] = { 192, 168, 51, 1 };
-static byte subnet[] = { 255, 255, 255, 0 };
-static byte dns_server[] = { 192, 168, 51, 1 };
-
-static char dispositivo_nome[10]   = "if-toque";
-static char dispositivo_local[10]  = "IFRN-SPP";
-static char dispositivo_versao[15] = "v2 b2021021011";
-time_t ultima_sincronizacao  = 0;
-
-// NTP Servers:
-IPAddress timeServer(10, 22, 0, 213);         // IP NTP LOCAL
-//IPAddress timeServer(200, 160, 0, 8);       // IP NTP PUBLICO   (ntp na internet ntp.br)
+void    Uptime();                                               // Calcula o tempo que o dispositivo se encontra ligado
+void    timeSyncCallback(struct timeval *tv);                   // Callback para obter momento de sincronização NTP
 
 
-// Variáveis Globais::
-// Para Controle de hora UTC
-const int timeZone = -3;                // Timezone do Brasil (nordeste)
-unsigned int localPort = 8888;          // Porta de comunicação UDP local do cliente NTP
-// Para exibicao em web: Controle de tempo para proximo toque
-tmElements_t tmToque;                    // Estrutura que armazena (hh, mm, ss) para o proximo toque
-tmElements_t tmRestante;                 // Estrutura que armazena (hh, mm, ss) restante para o proximo toque
+void setup(){  
+  pinMode(LED_AZUL_STATUS,OUTPUT);
+  pinMode(SINETA,OUTPUT);
+  pinMode(SINETA_TESTE,OUTPUT);
 
-// Para exibicao da hora atual em hh:mm:ss
-time_t prevDisplay = 0;                 // Controla para só exibir da data/hora no display/serial caso a hora atual seja diferente da atual
+  digitalWrite(LED_AZUL_STATUS, LOW);
+  digitalWrite(SINETA_TESTE, HIGH); //sempre ligada
+  digitalWrite(SINETA, LOW); // relé que controla a sineta / sirene / campainha escolar
+  sntp_set_time_sync_notification_cb(timeSyncCallback);
+    
+  Serial.begin(115200);
+  WiFi.disconnect(true); // delete old config
+  delay(2000);
+  
+  WiFi.onEvent(WiFiStationConnected, SYSTEM_EVENT_STA_CONNECTED);
+  WiFi.onEvent(WiFiGotIP, SYSTEM_EVENT_STA_GOT_IP);
+  WiFi.onEvent(WiFiStationDisconnected, SYSTEM_EVENT_STA_DISCONNECTED);
+ 
+  WiFi.begin(ssid, password);
+  Serial.print("ESP Board MAC Address:  ");
+  Serial.println(WiFi.macAddress());
+    
+  Serial.println("Iniciado");
+  Serial.println("Wait for WiFi... ");
 
-// Para controle de aula e turno
-byte atualAula = 0;                     // Armazena a Aula atual  : 1 a 6 ou 0, 10, 20 ou 30
-byte proxAula = 0;                      // Armazena a Proxima aula: 1 a 6 (nao considera intervalo como aula)
-byte turno = 0;                         // Armazena o turno atual
+  delay(4000);
+  server.begin();
 
-// Para Calculo de uptime do dispositivo
-static uint32_t UpTime = 0;             // Tempo em segundos que o dispotivo está ligado
-uint32_t prevMillis = millis();         // Millis() retorna o tempo que o arduino está ligado em ms. PrevMillis é incrementado a cada 1000ms (1s)
+  doc["projeto"]; 
 
+  doc["projeto"]["nome"]       = "IF-Toque";
+  doc["projeto"]["versao"]     = "v2.5";
+  doc["projeto"]["inicio"]     = "Nov/2017";
+  doc["projeto"]["compilacao"] = "27/09/2022";
+  
+  doc["sistema"];
+  doc["sistema"]["mac"]  = WiFi.macAddress();
+  doc["sistema"]["ip"]   = WiFi.localIP();
 
-EthernetUDP Udp;
-EthernetServer server(HTTP_PORT);
-
-void setup(){
-  pinMode(SINETA, OUTPUT);
-  digitalWrite(SINETA, LOW);
-  Serial.begin(9600);
-  delay(500);
-  Ethernet.begin(mac, ip, dns_server, gateway, subnet);
-  server.begin(); 
-  Serial.print(F("IP do dispositivo: "));
-  Serial.println(Ethernet.localIP());
-  Udp.begin(localPort);
-  Serial.print(F("Tentando sincronizar hora com NTP Server... "));
-  setSyncProvider(getNtpTime);
-  Serial.println(Ethernet.localIP());
-
-  if (timeStatus() == timeSet) {
-    ultima_sincronizacao = now();
-  }
-
+  doc["ntp"]["ntp_server"] = ntpServer;
+  doc["ntp"]["ultima_sincr_unixtime"] = 0;
+  
+  // Init and get the time
+  configTime(gmtOffset*3600, daylightOffset_sec, ntpServer);
+  printLocalTime();
 }
 
-
-void loop() {
-  if (timeToStudy(hour(), minute(), second())) {
-    digitalWrite(SINETA, HIGH); // relé que controla a sineta / sirene / campainha escolar
-  }
-  else {
-    digitalWrite(SINETA, LOW); // relé que controla a sineta / sirene / campainha escolar
-  }
-  
-  if (timeStatus() == timeSet) { // timeStatus() retorna: 0 timeNotSet, 1 timeNeedsSync ou 2 timeSet
-    Imprime_Info_DataHora_Serial();
-  }
-  else {
-    Serial.println(F("Obtencao de hora falhou. Verificar NTP/rede "));
-    Serial.println(F("Tentando sincronizar hora novamente... "));
-    delay(1000);
-    Udp.begin(localPort);
-    setSyncProvider(getNtpTime);
-    if (timeStatus() == timeSet) {
-      ultima_sincronizacao = now();
-    }
-  }  
+void loop(){
+  Uptime();
+  Imprime_Info_DataHora_Serial();
   ServidorWeb();
-  Uptime_Calc();
+  
+  if (getTime()){
+    digitalWrite(LED_AZUL_STATUS, HIGH);
+    if (timeToStudy(timeinfo.tm_hour,timeinfo.tm_min, timeinfo.tm_sec)){
+      digitalWrite(SINETA, HIGH); // relé que controla a sineta / sirene / campainha escolar
+      Serial.println("Toque... ");
+    }
+    else
+      digitalWrite(SINETA, LOW); // relé que controla a sineta / sirene / campainha escolar
+  }
+  else{
+    Serial.println("Falha ao tentar obter a hora");
+    digitalWrite(LED_AZUL_STATUS, LOW);
+    Serial.println("Tentando sincronizar");
+    configTime(gmtOffset*3600, daylightOffset_sec, ntpServer);
+  }
+    
 }
